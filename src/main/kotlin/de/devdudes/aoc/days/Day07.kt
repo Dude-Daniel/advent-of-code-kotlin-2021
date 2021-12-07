a439a5a9ae46bf8c12ce18810d7e517d0daaf74b
Daniel Hildebrandt <daniel.devdudes@googlemail.com>
2021-12-07 22:51:10 +0100

    Solution for day 07

--- src/main/kotlin/de/devdudes/aoc/days/Day07.kt
+++ src/main/kotlin/de/devdudes/aoc/days/Day07.kt
@@ -2,30 +2,47 @@ package de.devdudes.aoc.days
 
 import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
+import kotlin.math.abs
 
-class Day07 : Day(description = 7 - "Unknown", ignored = true, {
+class Day07 : Day(description = 7 - "The Treachery of Whales", {
 
     puzzle(
-        description = 1 - "Unknown",
+        description = 1 - "1 fuel per step",
         input = "day07",
         testInput = "day07_test",
-        expectedTestResult = Unit,
-        solutionResult = Unit,
+        expectedTestResult = 37,
+        solutionResult = 357353,
         solution = { input ->
-            // not implemented yet
-            TODO()
+            val crabPositions = input.first().split(",").map { it.toInt() }
+            val fuelCosts = crabPositions.calculateAllFuelCosts { stepCount ->
+                // one fuel per step
+                stepCount
+            }
+            fuelCosts.minOf { it }
         }
     )
 
     puzzle(
-        description = 2 - "Unknown",
+        description = 2 - "incremental fuel per step",
         input = "day07",
         testInput = "day07_test",
-        expectedTestResult = Unit,
-        solutionResult = Unit,
+        expectedTestResult = 168,
+        solutionResult = 104822130,
         solution = { input ->
-            // not implemented yet
-            TODO()
+            val crabPositions = input.first().split(",").map { it.toInt() }
+            val fuelCosts = crabPositions.calculateAllFuelCosts { stepCount ->
+                // one fuel for 1st step, 2 for 2nd step, etc.
+                (1..stepCount).sum()
+            }
+            fuelCosts.minOf { it }
         }
     )
 })
+
+private fun List<Int>.calculateAllFuelCosts(calculator: (stepCount: Int) -> Int): List<Int> {
+    val maxPosition = maxOf { it }
+    return (1..maxPosition).map { position -> calculateFuelCost(position, calculator) }
+}
+
+private fun List<Int>.calculateFuelCost(position: Int, calculator: (steps: Int) -> Int): Int =
+    sumOf { calculator(abs(it - position)) }
