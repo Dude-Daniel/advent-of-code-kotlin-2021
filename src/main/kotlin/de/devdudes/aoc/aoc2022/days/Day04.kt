712b05afd6af97e19ef66aae3c09b26217a2759c
Daniel Hildebrandt <d.hildebrandt@reply.de>
2022-12-05 22:54:06 +0100

    Add: Solution for day 04 2022

--- src/main/kotlin/de/devdudes/aoc/aoc2022/days/Day04.kt
+++ src/main/kotlin/de/devdudes/aoc/aoc2022/days/Day04.kt
@@ -4,30 +4,45 @@ import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
 
 class Day04 : Day(
-    description = 4 - "Unknown",
-    ignored = true,
+    description = 4 - "Camp Cleanup",
+    ignored = false,
     days = {
         puzzle(
-            description = 1 - "Unknown",
+            description = 1 - "number of ranges contained in another",
             input = "day04",
             testInput = "day04_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 2,
+            solutionResult = 534,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                val ranges = input.map { line ->
+                    line.split(",").map { range ->
+                        val (start, end) = range.split("-").map { it.toInt() }
+                        start.rangeTo(end)
+                    }
+                }
+                ranges.count { (first, second) ->
+                    val intersections = first.intersect(second)
+                    first.count() <= intersections.count() || second.count() <= intersections.count()
+                }
             }
         )
 
         puzzle(
-            description = 2 - "Unknown",
+            description = 2 - "number of overlaps",
             input = "day04",
             testInput = "day04_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 4,
+            solutionResult = 841,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                val ranges = input.map { line ->
+                    line.split(",").map { range ->
+                        val (start, end) = range.split("-").map { it.toInt() }
+                        start.rangeTo(end)
+                    }
+                }
+                ranges.count { (first, second) ->
+                    first.intersect(second).isNotEmpty()
+                }
             }
         )
     }
