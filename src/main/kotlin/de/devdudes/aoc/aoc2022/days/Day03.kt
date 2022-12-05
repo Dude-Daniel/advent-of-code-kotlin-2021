c3eaa71876b02028a26345cba257efaa33206212
Daniel Hildebrandt <d.hildebrandt@reply.de>
2022-12-05 22:25:51 +0100

    Add: Solution for day 03 2022

--- src/main/kotlin/de/devdudes/aoc/aoc2022/days/Day03.kt
+++ src/main/kotlin/de/devdudes/aoc/aoc2022/days/Day03.kt
@@ -2,33 +2,52 @@ package de.devdudes.aoc.aoc2022.days
 
 import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
+import java.lang.UnsupportedOperationException
 
 class Day03 : Day(
-    description = 3 - "Unknown",
-    ignored = true,
+    description = 3 - "Rucksack Reorganization",
+    ignored = false,
     days = {
         puzzle(
-            description = 1 - "Unknown",
+            description = 1 - "sum of the priorities of duplicate items per backpack",
             input = "day03",
             testInput = "day03_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 157,
+            solutionResult = 7691,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                val priorities = input.map { backpack ->
+                    val compartmentSize = backpack.length / 2
+                    val (c1, c2) = backpack.chunked(compartmentSize)
+                    c1.findAnyOf(c2.chunked(1))!!.second.priority
+                }
+                priorities.sum()
             }
         )
 
         puzzle(
-            description = 2 - "Unknown",
+            description = 2 - "sum of the priorities of elf group badges",
             input = "day03",
             testInput = "day03_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 70,
+            solutionResult = 2508,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                val groups = input.chunked(3)
+                val groupPriorities = groups
+                    .map { backpacks ->
+                        backpacks.map { it.toCharArray().toSet() }
+                    }
+                    .map { (b1, b2, b3) ->
+                        b1.intersect(b2).intersect(b3).first().toString().priority
+                    }
+                groupPriorities.sum()
             }
         )
     }
 )
+
+private val String.priority: Int
+    get() = when (val char = this.toCharArray().first()) {
+        in 'a'..'z' -> char.code - 96
+        in 'A'..'Z' -> char.code - 38
+        else -> throw IllegalArgumentException("char is not a priority: $char")
+    }
