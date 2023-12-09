4665388518c8f3ad42a4cbeb9c9f519cc1c8b900
Daniel Hildebrandt <d.hildebrandt@reply.de>
2023-12-09 15:54:53 +0100

    Add: Solution for day 01 2015

--- src/main/kotlin/de/devdudes/aoc/aoc2015/days/Day01.kt
+++ src/main/kotlin/de/devdudes/aoc/aoc2015/days/Day01.kt
@@ -4,30 +4,32 @@ import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
 
 class Day01 : Day(
-    description = 1 - "Unknown",
-    ignored = true,
+    description = 1 - "Not Quite Lisp",
+    ignored = false,
     days = {
         puzzle(
-            description = 1 - "Unknown",
+            description = 1 - "Calculate Floor",
             input = "day01",
             testInput = "day01_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = -3L,
+            solutionResult = 232L,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                input.first().sumOf { if (it == '(') 1L else -1L }
             }
         )
 
         puzzle(
-            description = 2 - "Unknown",
+            description = 2 - "Character Position to enter basement",
             input = "day01",
             testInput = "day01_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 1,
+            solutionResult = 1783,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                var counter = 0L
+                input.first().indexOfFirst {
+                    counter += if (it == '(') 1L else -1L
+                    counter < 0
+                } + 1
             }
         )
     }
