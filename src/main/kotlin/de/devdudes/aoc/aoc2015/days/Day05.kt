f1d7b23e5af63ddcbd9aee2c80d63c8b349ef52c
Daniel Hildebrandt <d.hildebrandt@reply.de>
2023-12-09 18:39:54 +0100

    Add: Solution for day 05 2015

--- src/main/kotlin/de/devdudes/aoc/aoc2015/days/Day05.kt
+++ src/main/kotlin/de/devdudes/aoc/aoc2015/days/Day05.kt
@@ -2,33 +2,73 @@ package de.devdudes.aoc.aoc2015.days
 
 import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
+import kotlin.streams.asSequence
 
 class Day05 : Day(
-    description = 5 - "Unknown",
-    ignored = true,
+    description = 5 - "Doesn't He Have Intern-Elves For This? - Number of nice Strings",
+    ignored = false,
     days = {
         puzzle(
-            description = 1 - "Unknown",
+            description = 1 - "vowels, double letter and illegal texts",
             input = "day05",
             testInput = "day05_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 2,
+            solutionResult = 255,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                NiceTextFinderOne(input).findNiceTexts().size
             }
         )
 
         puzzle(
-            description = 2 - "Unknown",
+            description = 2 - "pairs appearing twice and same letter with one letter in between",
             input = "day05",
             testInput = "day05_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 2,
+            solutionResult = 55,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                NiceTextFinderTwo(input).findNiceTexts().size
             }
         )
     }
 )
+
+private data class NiceTextFinderOne(val texts: List<String>) {
+
+    fun findNiceTexts(): List<String> =
+        texts.filter { text ->
+            val vowelCount = text.filter { it in VOWELS }.length
+            val doubleLetter = text.chars().asSequence()
+                .windowed(size = 2, step = 1)
+                .firstOrNull { (a, b) -> a == b }
+            val illegalText = ILLEGAL_TEXTS.firstOrNull { text.contains(it) }
+
+            vowelCount >= 3 && doubleLetter != null && illegalText == null
+        }
+
+    private companion object {
+        private const val VOWELS = "aeiou"
+        private val ILLEGAL_TEXTS = listOf("ab", "cd", "pq", "xy")
+    }
+}
+
+private data class NiceTextFinderTwo(val texts: List<String>) {
+
+    fun findNiceTexts(): List<String> =
+        texts.filter { text ->
+            // find all letter pairs
+            val letterPairs = text.windowed(size = 2, step = 1).toSet()
+            val doubleLetterPair = letterPairs.firstOrNull { pair ->
+                // for each letter pair find one that is not overlapping
+                // (always true if the letter pair exists at least three times
+                // or if their starting index is two apart)
+                val occurrences = Regex(pair).findAll(text).map { it.range }.toList()
+                occurrences.size > 3 || (occurrences.size == 2 && occurrences[0].first + 1 < occurrences[1].first)
+            }
+
+            val letterWithOneInBetween = text.chars().asSequence()
+                .windowed(size = 3, step = 1)
+                .firstOrNull { (a, _, b) -> a == b }
+
+            doubleLetterPair != null && letterWithOneInBetween != null
+        }
+}
