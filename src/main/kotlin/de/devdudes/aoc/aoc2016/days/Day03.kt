8a2f4260b7a91a347da9a9306fcc1e34d8fa5c04
Daniel Hildebrandt <daniel.devdudes@googlemail.com>
2024-11-25 23:00:48 +0100

    Add: Solution for day 03 2016

--- src/main/kotlin/de/devdudes/aoc/aoc2016/days/Day03.kt
+++ src/main/kotlin/de/devdudes/aoc/aoc2016/days/Day03.kt
@@ -2,33 +2,60 @@ package de.devdudes.aoc.aoc2016.days
 
 import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
+import de.devdudes.aoc.helpers.transpose
 
 class Day03 : Day(
-    description = 3 - "Unknown",
-    ignored = true,
+    description = 3 - "Squares With Three Sides",
+    ignored = false,
     days = {
         puzzle(
-            description = 1 - "Unknown",
+            description = 1 - "One Triangle per Row",
             input = "day03",
             testInput = "day03_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 2,
+            solutionResult = 982,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                parseTrianglesPerRow(input).countValidTriangles()
             }
         )
 
         puzzle(
-            description = 2 - "Unknown",
+            description = 2 - "Vertical Triangles across Rows",
             input = "day03",
             testInput = "day03_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 2,
+            solutionResult = 1826,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                parseTrianglesVerticallyAcrossRows(input).countValidTriangles()
+
             }
         )
     }
 )
+
+private fun parseTrianglesPerRow(input: List<String>): List<Triangle> =
+    input.map { triangleString ->
+        val (a, b, c) = triangleString.split(" ").filter { it.isNotBlank() }
+        Triangle(a.toInt(), b.toInt(), c.toInt())
+    }
+
+/**
+ * 1. Parse input and convert to list of ints: [1, 2, 3]
+ * 2. Transpose the input, so columns become rows -> 3 rows with a lot of elements
+ * 3. Flatten the 3 rows to a single row
+ * 4. Take chunks of 3 elements and consider them as one Triangle
+ */
+private fun parseTrianglesVerticallyAcrossRows(input: List<String>): List<Triangle> =
+    input.map { triangleString ->
+        val (a, b, c) = triangleString.split(" ").filter { it.isNotBlank() }
+        listOf(a.toInt(), b.toInt(), c.toInt())
+    }.transpose()
+        .flatten()
+        .chunked(3)
+        .map { (a, b, c) -> Triangle(a, b, c) }
+
+private data class Triangle(val a: Int, val b: Int, val c: Int) {
+    val isValid: Boolean = a + b > c && a + c > b && b + c > a
+}
+
+private fun List<Triangle>.countValidTriangles(): Int = count { triangle -> triangle.isValid }
