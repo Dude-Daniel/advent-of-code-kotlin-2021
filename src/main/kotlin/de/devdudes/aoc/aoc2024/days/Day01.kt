cc78529dfa4265d7fa8df4394b40c0b677eca6bb
Daniel Hildebrandt <daniel.devdudes@googlemail.com>
2024-12-01 13:18:19 +0100

    Add: Solution for day 01 2024

--- src/main/kotlin/de/devdudes/aoc/aoc2024/days/Day01.kt
+++ src/main/kotlin/de/devdudes/aoc/aoc2024/days/Day01.kt
@@ -2,32 +2,47 @@ package de.devdudes.aoc.aoc2024.days
 
 import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
+import de.devdudes.aoc.helpers.transpose
+import kotlin.math.abs
 
 class Day01 : Day(
-    description = 1 - "Unknown",
-    ignored = true,
+    description = 1 - "Historian Hysteria",
+    ignored = false,
     days = {
         puzzle(
-            description = 1 - "Unknown",
+            description = 1 - "Total Distance",
             input = "day01",
             testInput = "day01_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 11,
+            solutionResult = 1_579_939,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                input.map { row -> row.split("   ").map(String::toInt) }
+                    .transpose() // transpose so we have 2 lists
+                    .map(List<Int>::sorted) // sort each list
+                    .transpose() // transpose back to original representation
+                    .sumOf { abs(it.first() - it.last()) } // sum of absolute differences
             }
         )
 
         puzzle(
-            description = 2 - "Unknown",
+            description = 2 - "Similarity Score",
             input = "day01",
             testInput = "day01_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 31,
+            solutionResult = 20_351_745,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                val (leftList, rightList) =
+                    input.map { row -> row.split("   ").map(String::toInt) }
+                        .transpose() // transpose so we have 2 lists
+
+                val idsWithCount = leftList.groupBy { it } // group by id number
+                    .mapValues { it.value.size } // count the number of occurrences
+                    .toList() // convert to list of pairs (id, count)
+
+                idsWithCount.sumOf { (id, count) ->
+                    val similarityScore = id * rightList.count { rightId -> rightId == id }
+                    similarityScore * count
+                }
             }
         )
     }
