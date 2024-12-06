805cb1a43cb0f60ede808fb4c40bc45a6fd3ea1a
Daniel Hildebrandt <daniel.devdudes@googlemail.com>
2024-12-06 11:15:52 +0100

    Add: Solution for day 05 2024

--- src/main/kotlin/de/devdudes/aoc/aoc2024/days/Day05.kt
+++ src/main/kotlin/de/devdudes/aoc/aoc2024/days/Day05.kt
@@ -2,33 +2,68 @@ package de.devdudes.aoc.aoc2024.days
 
 import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
+import de.devdudes.aoc.helpers.splitWhen
 
 class Day05 : Day(
-    description = 5 - "Unknown",
-    ignored = true,
+    description = 5 - "Print Queue",
+    ignored = false,
     days = {
         puzzle(
-            description = 1 - "Unknown",
+            description = 1 - "Sum correctly ordered Page Updates",
             input = "day05",
             testInput = "day05_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 143,
+            solutionResult = 5_651,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                parsePrintQueueData(input).calculateSumOfValidUpdates()
             }
         )
 
         puzzle(
-            description = 2 - "Unknown",
+            description = 2 - "Recalculate and Sum incorrectly ordered Page Updates",
             input = "day05",
             testInput = "day05_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 123,
+            solutionResult = 4_743,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                parsePrintQueueData(input).calculateSumOfInvalidUpdates()
             }
         )
     }
 )
+
+private fun parsePrintQueueData(input: List<String>): PrintQueueData {
+    val (orderingRulesRaw, updatesRaw) = input.splitWhen { it.isEmpty() }
+
+    val orderingRules = orderingRulesRaw.map {
+        val (from, to) = it.split("|")
+        from.toInt() to to.toInt()
+    }
+
+    val updates = updatesRaw.map { it.split(",").map(String::toInt) }
+
+    return PrintQueueData(orderingRules = orderingRules, updates = updates)
+}
+
+private data class PrintQueueData(
+    val orderingRules: List<Pair<Int, Int>>,
+    val updates: List<List<Int>>,
+) {
+
+    private fun filterUpdates(valid: Boolean): List<List<Int>> =
+        updates.filter { pages ->
+            valid == pages.zipWithNext().all { orderingRule -> orderingRules.contains(orderingRule) }
+        }
+
+    fun calculateSumOfValidUpdates(): Int = filterUpdates(true).sumOf { pages -> pages[pages.lastIndex / 2] }
+
+    fun calculateSumOfInvalidUpdates(): Int =
+        filterUpdates(false)
+            .map { pages ->
+                // resort the pages by checking how two compared values are defined by the ordering rules
+                pages.sortedWith { first, second ->
+                    if (orderingRules.contains(first to second)) -1 else 1
+                }
+            }
+            .sumOf { pages -> pages[pages.lastIndex / 2] }
+}
