d0c4a6eaa26b836430b880b6cc192497a3eebc38
Daniel Hildebrandt <daniel.devdudes@googlemail.com>
2024-12-07 12:13:55 +0100

    Add: Solution for day 07 2024

--- src/main/kotlin/de/devdudes/aoc/aoc2024/days/Day07.kt
+++ src/main/kotlin/de/devdudes/aoc/aoc2024/days/Day07.kt
@@ -2,33 +2,66 @@ package de.devdudes.aoc.aoc2024.days
 
 import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
+import de.devdudes.aoc.helpers.concat
 
 class Day07 : Day(
-    description = 7 - "Unknown",
-    ignored = true,
+    description = 7 - "Bridge Repair - Solve Equations",
+    ignored = false,
     days = {
         puzzle(
-            description = 1 - "Unknown",
+            description = 1 - "Add and Multiply",
             input = "day07",
             testInput = "day07_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 3_749L,
+            solutionResult = 663_613_490_587L,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                BridgeCalibration(input).sumResultsOfValidEquations(supportsConcatinationOperators = false)
             }
         )
 
         puzzle(
-            description = 2 - "Unknown",
+            description = 2 - "Add, Multiply and Concatenate",
             input = "day07",
             testInput = "day07_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 11_387L,
+            solutionResult = 110_365_987_435_001L,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                BridgeCalibration(input).sumResultsOfValidEquations(supportsConcatinationOperators = true)
             }
         )
     }
 )
+
+private data class CalibrationEquation(val result: Long, val numbers: List<Long>)
+
+private class BridgeCalibration(private val input: List<String>) {
+
+    private fun parseCalibrations(): List<CalibrationEquation> =
+        input.map { line ->
+            val (result, numbers) = line.split(": ")
+            val numbersList = numbers.split(" ").map { it.toLong() }
+            CalibrationEquation(result.toLong(), numbersList)
+        }
+
+    fun sumResultsOfValidEquations(supportsConcatinationOperators: Boolean): Long =
+        parseCalibrations()
+            .sumOf { equation ->
+                val first = equation.numbers.first()
+                val allButFirst = equation.numbers.drop(1)
+
+                val equationResults = allButFirst.fold(listOf(first)) { acc, number ->
+                    buildList {
+                        acc.forEach { value ->
+                            if (value + number <= equation.result) add(value + number)
+                            if (value * number <= equation.result) add(value * number)
+
+                            if (supportsConcatinationOperators) {
+                                val accValue = value concat number
+                                if (accValue <= equation.result) add(accValue)
+                            }
+                        }
+                    }
+                }
+                if (equationResults.any { it == equation.result }) equation.result else 0
+            }
+}
--- src/main/kotlin/de/devdudes/aoc/helpers/Math.kt
+++ src/main/kotlin/de/devdudes/aoc/helpers/Math.kt
@@ -14,3 +14,25 @@ fun gcd(a: Long, b: Long): Long {
 fun lcm(a: Long, b: Long): Long {
     return a / gcd(a, b) * b
 }
+
+/**
+ * Concatenates two numbers. i.e. 12 and 345 becomes 12345.
+ */
+infix fun Int.concat(other: Int): Int {
+    var aScale = 1
+    while (aScale <= other) {
+        aScale *= 10
+    }
+    return this * aScale + other
+}
+
+/**
+ * Concatenates two numbers. i.e. 12 and 345 becomes 12345.
+ */
+infix fun Long.concat(other: Long): Long {
+    var aScale = 1L
+    while (aScale <= other) {
+        aScale *= 10L
+    }
+    return this * aScale + other
+}
