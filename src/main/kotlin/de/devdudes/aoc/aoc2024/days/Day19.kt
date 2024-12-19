73290bc4283bf85399f6c554cf1668c4fe0c31e0
Daniel Hildebrandt <daniel.devdudes@googlemail.com>
2024-12-19 11:54:20 +0100

    Add: Solution for day 19 2024

--- src/main/kotlin/de/devdudes/aoc/aoc2024/days/Day19.kt
+++ src/main/kotlin/de/devdudes/aoc/aoc2024/days/Day19.kt
@@ -4,31 +4,62 @@ import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
 
 class Day19 : Day(
-    description = 19 - "Unknown",
-    ignored = true,
+    description = 19 - "Linen Layout",
+    ignored = false,
     days = {
         puzzle(
-            description = 1 - "Unknown",
+            description = 1 - "Number of possible Designs",
             input = "day19",
             testInput = "day19_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 6,
+            solutionResult = 340,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                TowelArrangementSolver(parseTowelConfig(input)).countValidDesigns()
             }
         )
 
         puzzle(
-            description = 2 - "Unknown",
+            description = 2 - "Total Number of possible Arrangements",
             input = "day19",
             testInput = "day19_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 16L,
+            solutionResult = 717_561_822_679_428L,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                TowelArrangementSolver(parseTowelConfig(input)).countTotalNumberOfPossibleDesigns()
             }
         )
     }
 )
+
+private fun parseTowelConfig(input: List<String>): TowelConfig =
+    TowelConfig(
+        patterns = input.first().split(", "),
+        designs = input.drop(2),
+    )
+
+private data class TowelConfig(
+    val patterns: List<String>,
+    val designs: List<String>,
+)
+
+private class TowelArrangementSolver(private val config: TowelConfig) {
+
+    private val cachedCounts = mutableMapOf<String, Long>()
+
+    fun countValidDesigns(): Int = config.designs.count { countPossibleBuildDesigns(it) > 0L }
+
+    fun countTotalNumberOfPossibleDesigns(): Long = config.designs.sumOf { countPossibleBuildDesigns(it) }
+
+    private fun countPossibleBuildDesigns(design: String): Long {
+        if (design.isEmpty()) return 1L
+        val cachedCount = cachedCounts[design]
+        if (cachedCount != null) return cachedCount
+        val count = config.patterns.sumOf { pattern ->
+            if (design.startsWith(pattern)) {
+                countPossibleBuildDesigns(design.drop(pattern.length))
+            } else 0L
+        }
+        cachedCounts[design] = count
+        return count
+    }
+}
