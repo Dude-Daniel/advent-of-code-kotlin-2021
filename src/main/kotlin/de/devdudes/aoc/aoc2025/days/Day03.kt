36eee351cbb4968bee08bf6c678ea6d79a9772d3
Daniel Hildebrandt <daniel.devdudes@googlemail.com>
2025-12-03 09:48:12 +0100

    Add: Solution for day 03 2025

--- src/main/kotlin/de/devdudes/aoc/aoc2025/days/Day03.kt
+++ src/main/kotlin/de/devdudes/aoc/aoc2025/days/Day03.kt
@@ -2,33 +2,63 @@ package de.devdudes.aoc.aoc2025.days
 
 import de.devdudes.aoc.core.Day
 import de.devdudes.aoc.core.minus
+import de.devdudes.aoc.helpers.concat
 
 class Day03 : Day(
-    description = 3 - "Unknown",
-    ignored = true,
+    description = 3 - "Lobby",
+    ignored = false,
     days = {
         puzzle(
-            description = 1 - "Unknown",
+            description = 1 - "Maximum joltage using 2 Batteries",
             input = "day03",
             testInput = "day03_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 357L,
+            solutionResult = 17_430L,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                BatterySolver(input).findMaxJoltage(batteryCount = 2)
             }
         )
 
         puzzle(
-            description = 2 - "Unknown",
+            description = 2 - "Maximum joltage using 12 Batteries",
             input = "day03",
             testInput = "day03_test",
-            expectedTestResult = Unit,
-            solutionResult = Unit,
+            expectedTestResult = 3_121_910_778_619L,
+            solutionResult = 171_975_854_269_367L,
             solution = { input ->
-                // not implemented yet
-                TODO()
+                BatterySolver(input).findMaxJoltage(batteryCount = 12)
             }
         )
     }
 )
+
+private class BatterySolver(input: List<String>) {
+
+    private val batteryBanks: List<BatteryBank> = input.map { BatteryBank(it.toCharArray().map(Char::digitToInt)) }
+
+    fun findMaxJoltage(batteryCount: Int): Long = batteryBanks.sumOf { it.maxJoltage(batteryCount) }
+
+    private data class BatteryBank(val joltages: List<Int>) {
+
+        fun maxJoltage(batteryCount: Int): Long = findNextBatteryJoltage(joltages, batteryCount)
+
+        private fun findNextBatteryJoltage(batteries: List<Int>, batteriesToUse: Int): Long {
+            // find max value if the given battery input. Ignore the last batteries so they can be picked up in the next recursive iteration.
+            val maxBatteryValue = batteries.dropLast(batteriesToUse - 1).max()
+
+            val index = batteries.indexOf(maxBatteryValue)
+
+            return if (batteriesToUse <= 1) {
+                maxBatteryValue.toLong()
+            } else {
+                // find the next part of the joltage value by recursively evaluating the remaining joltages (all batteries after the found battery)
+                val maxSecondValue = findNextBatteryJoltage(
+                    batteries = batteries.drop(index + 1),
+                    batteriesToUse = batteriesToUse - 1,
+                )
+
+                maxBatteryValue.toLong().concat(maxSecondValue)
+            }
+        }
+    }
+}
